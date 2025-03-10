e7193f7bf7983b8bda3b64527f8ba34f48d2d20e
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-03-10 23:59:02 +0700

    video 49

--- src/index.js
+++ src/index.js
@@ -19,9 +19,11 @@ root.render(
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="users" element={<User />} />
-        <Route path="admins" element={<Admin />} />
+        <Route path="/" element={<App />} >
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
