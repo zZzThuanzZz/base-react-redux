9e3767102e8e921316a5c2b856919a663f79ef04
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-03-13 08:08:09 +0700

    video 54

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -13,7 +13,7 @@ const HomePage = (props) => {
                 <div className='title-1'>There's a better way to ask</div>
                 <div className='title-2'>You don't want to make a boring form.
                     And your audience won't answer one.
-                    Create a typeform instead-and make everyone happy.
+                    Create a typeform instead - and make everyone happy.
                 </div>
                 <div className='title-3'>
                     <button>Get's started. It's free</button>
