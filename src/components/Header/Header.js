78b65d205dbde6aa1e3b13580986ac51ca76be58
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-04-17 15:39:42 +0700

    video 78

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated)
+    const account = useSelector(state => state.user.account)
     const navigate = useNavigate();
     const handleLogin = () => {
         navigate('/login')
@@ -27,13 +30,17 @@ const Header = () => {
                         <Nav.Link href="users">Users</Nav.Link>
                         <Nav.Link href="admins">Admin</Nav.Link> */}
                     </Nav>
-                    <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
-                    <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
-                    {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
-                        <NavDropdown.Item>Log in</NavDropdown.Item>
-                        <NavDropdown.Item>Log out</NavDropdown.Item>
-                        <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </NavDropdown> */}
+                    {isAuthenticated === false ?
+                        <>
+                            <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
+                            <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
+                        </>
+                        :
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Log out</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                        </NavDropdown>
+                    }
                     <Nav>
 
                     </Nav>
