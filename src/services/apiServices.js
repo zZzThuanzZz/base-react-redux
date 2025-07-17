da19bf51d2924d734ff22aaa3d55d20562d402a5
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-07-17 16:25:34 +0700

    video 88

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -59,8 +59,12 @@ const getQuizByUser = () => {
 const getDataQuiz = (id) => {
     return axios.get(`/api/v1/questions-by-quiz?quizId=${id}`);
 }
+
+const postSubmitQuiz = (data) => {
+    return axios.post(`/api/v1/quiz-submit`, { ...data });
+}
 export {
     postCreateNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin, postRegister,
-    getQuizByUser, getDataQuiz
+    getQuizByUser, getDataQuiz, postSubmitQuiz
 }
