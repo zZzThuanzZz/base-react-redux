60d5d3b0e4cb78ce49f1b740efecb0eb94971c36
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-12-11 08:36:24 +0700

    video 110

--- src/Layout.js
+++ src/Layout.js
@@ -6,7 +6,7 @@ import User from './components/User/User';
 import Admin from './components/Admin/Admin';
 import HomePage from './components/Home/HomePage';
 import ManageUser from './components/Admin/Content/ManageUser';
-import Dashboard from './components/Admin/Content/Dashboard';
+import DashBoard from './components/Admin/Content/DashBoard';
 import Login from './components/Auth/Login';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
@@ -46,7 +46,7 @@ const Layout = (props) => {
                         <Admin />
                     </PrivateRoute>
                 } >
-                    <Route index element={<Dashboard />} />
+                    <Route index element={<DashBoard />} />
                     <Route path="manage-users" element={<ManageUser />} />
                     <Route path="manage-quizzes" element={<ManageQuiz />} />
                     <Route path="manage-questions" element={<Questions />} />
--- src/components/Admin/Content/Dashboard.js
+++ src/components/Admin/Content/Dashboard.js
@@ -1,8 +1,69 @@
-const Dashboard = (props) => {
+import './DashBoard.scss';
+import { BarChart, Legend, XAxis, YAxis, CartesianGrid, Tooltip, Bar } from 'recharts';
+const DashBoard = (props) => {
+    const data = [
+        {
+            name: 'Page A',
+            uv: 4000,
+            pv: 2400,
+        },
+        {
+            name: 'Page B',
+            uv: 3000,
+            pv: 1398,
+        },
+        {
+            name: 'Page C',
+            uv: 2000,
+            pv: 9800,
+        },
+        {
+            name: 'Page D',
+            uv: 2780,
+            pv: 3908,
+        },
+        {
+            name: 'Page E',
+            uv: 1890,
+            pv: 4800,
+        },
+        {
+            name: 'Page F',
+            uv: 2390,
+            pv: 3800,
+        },
+        {
+            name: 'Page G',
+            uv: 3490,
+            pv: 4300,
+        },
+    ];
     return (
-        <div>
-            Dash board
+        <div className="dashboard-container">
+            <div className='title'>
+                Analytics Dashboard
+            </div>
+            <div className='content'>
+                <div className='c-left'>
+                    <div className='child'>Total Users</div>
+                    <div className='child'>Total Quizzes</div>
+                    <div className='child'>Total Questions</div>
+                    <div className='child'>Total Answers</div>
+                </div>
+                <div className='c-right'>
+                    <BarChart width={400} height={300} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" fill="#8884d8" />
+                        <Bar dataKey="uv" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+            </div>
+
         </div>
     )
 }
-export default Dashboard
+export default DashBoard;
