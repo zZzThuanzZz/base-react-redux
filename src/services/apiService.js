bdffc501458c758d56a4a15240773aaba2591e14
zZzThuanzZz <kid.thuan,nguyen@gmail.com>
2025-12-14 09:44:23 +0700

    video 111

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -118,17 +118,21 @@ const postAssignQuiz = (quizId, userId) => {
 }
 
 const getQuizWithQA = (quizId) => {
-    return axios.get(`/api/v1/quiz-with-qa/${quizId}`);
+    return axios.get(`api/v1/quiz-with-qa/${quizId}`);
 }
 
 const postUpsertQA = (data) => {
-    return axios.post(`/api/v1/quiz-upsert-qa`, { ...data });
+    return axios.post(`api/v1/quiz-upsert-qa`, { ...data });
 }
 
 const logout = (email, refresh_token) => {
     return axios.post('api/v1/logout', {
         email, refresh_token
     });
+}
+
+const getOverview = () => {
+    return axios.get(`api/v1/overview`);
 }
 
 export {
@@ -138,5 +142,5 @@ export {
     postSubmitQuiz, postCreateNewQuiz,
     getAllQuizForAdmin, putUpdateQuizForAdmin, deleteQuizForAdmin,
     postCreateNewQuestionForQuiz, postCreateNewAnswerForQuestion, postAssignQuiz,
-    getQuizWithQA, postUpsertQA, logout
+    getQuizWithQA, postUpsertQA, logout, getOverview
 }
